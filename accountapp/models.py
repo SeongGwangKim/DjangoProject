c81716806393ac4e6d2851b94a09e3ab140c056b
SeongGwangKim <78336335+SeongGwangKim@users.noreply.github.com>
2021-12-13 00:24:01 +0900

    [14] migrate

--- accountapp/models.py
+++ accountapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class HelloWorld(models.Model):
+    # models.CharField가 반드시 있어야 한다면 null=False로 지정한다 True이면 없어도 되는 것
+    text = models.CharField(max_length=255, null=False)
+
+
+
