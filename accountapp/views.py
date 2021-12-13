3c4fca84dceb432ffb054172352bd103c33673b5
SeongGwangKim <78336335+SeongGwangKim@users.noreply.github.com>
2021-12-13 22:35:12 +0900

    [16] request - POST, GET

--- accountapp/views.py
+++ accountapp/views.py
@@ -6,5 +6,11 @@ from django.shortcuts import render
 
 
 def hello_world(request):
-    # return HttpResponse('Hello World!')
-    return render(request, 'accountapp/hello_world.html')
+
+    # request받은 메소드가 POST일 경우에
+    if request.method == "POST":
+        # return HttpResponse('Hello World!')
+        #  context={'text': 'POST METHOD!!'} : text라는 이름을 가지고 있고 내용은 POST METHOD!!로 설정.
+        return render(request, 'accountapp/hello_world.html', context={'text': 'POST METHOD!!'})
+    else:
+        return render(request, 'accountapp/hello_world.html', context={'text': 'GET METHOD!!'})
