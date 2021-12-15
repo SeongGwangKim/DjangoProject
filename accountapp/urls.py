6c60200877ee2789517e1caf5805cfc9f64b0ed1
SeongGwangKim <78336335+SeongGwangKim@users.noreply.github.com>
2021-12-15 23:50:43 +0900

    [22] Implement login, logout

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.views import LogoutView, LoginView
 from django.urls import path
 
 from accountapp.views import hello_world, AccountCreateView
@@ -8,6 +9,11 @@ app_name = "accountapp"
 urlpatterns = [
     # 함수형 view는 함수형 view의 이름(hello_world)을 써주면 된다.
     path('hello_world/', hello_world, name='hello_world'),
+
+    path('login/', LoginView.as_view(template_name='accountapp/login.html'), name='login'),
+
+    path('logout/', LogoutView.as_view(), name='logout'),
+
     # 회원가입을 할 경로를 지정
     # class형 view는 class명.as_view()를 써줘야 한다.
     path('create/', AccountCreateView.as_view(), name='create')
--- main/settings.py
+++ main/settings.py
@@ -15,6 +15,8 @@ from pathlib import Path
 import environ
 import os
 
+from django.urls import reverse_lazy
+
 env = environ.Env(
     # set casting, default value
     DEBUG=(bool, False)
@@ -147,3 +149,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = reverse_lazy('accountapp:hello_world')
+
+LOGOUT_REDIRECT_URL = reverse_lazy('accountapp:login')
