ebe3fd54016346369842a9f8c431b8a5e0c3f56d
SeongGwangKim <78336335+SeongGwangKim@users.noreply.github.com>
2021-12-15 22:01:33 +0900

    [21] Create User account template

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 
-from accountapp.views import hello_world
-
+from accountapp.views import hello_world, AccountCreateView
 
 app_name = "accountapp"
 
 
 urlpatterns = [
-    path('hello_world/', hello_world, name='hello_world')
+    # 함수형 view는 함수형 view의 이름(hello_world)을 써주면 된다.
+    path('hello_world/', hello_world, name='hello_world'),
+    # 회원가입을 할 경로를 지정
+    # class형 view는 class명.as_view()를 써줘야 한다.
+    path('create/', AccountCreateView.as_view(), name='create')
 ]
--- accountapp/views.py
+++ accountapp/views.py
@@ -1,13 +1,16 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 
 # Create your views here.
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
+from django.views.generic import CreateView
 
 from accountapp.models import HelloWorld
 
-
+# 함수형 view
 def hello_world(request):
 
     # request받은 메소드가 POST일 경우에
@@ -32,3 +35,17 @@ def hello_world(request):
         # HelloWorld의 모든 데이터를 긁어 올 수 있다.
         hello_world_list = HelloWorld.objects.all()
         return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
+
+# class형 view
+class AccountCreateView(CreateView):
+    # 해당 패키지의 정보를 보고 싶으면 그 곳에 Ctrl + b를 눌러서 확인
+    model = User
+    # 만들 때 사용할 폼이 필요하다.
+    form_class = UserCreationForm
+    # 이 계정을 만들기에 성공을 했다면 어느 경로에 연결을 할 것인가 연결해줄 곳을 연결
+    # reverse_lazy는 class형 view에서 사용, reverse는 함수형 view에서 사용한다.
+    success_url = reverse_lazy('accountapp:hello_world')
+    # 회원가입을 할 때 볼 html 설정
+    template_name = 'accountapp/create.html'
+
+
